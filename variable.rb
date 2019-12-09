ac0b1c08d16957471d094a508d6a626f5b956757
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-09 23:57:47 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,3 @@
- # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
+def greeting
+  puts "Hello World" # Define a local variable called 'greeting',
+end # give it the value of the string "Hello World"
